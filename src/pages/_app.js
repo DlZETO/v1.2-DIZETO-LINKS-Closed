14a5cd90298421d363ca90e29379f6b59d1e2849
= <intanmeilanienugraha@gmail.com>
2023-04-22 22:51:44 +0700

    Update Package

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "dizeto/styles/tailwind.css";
 import "dizeto/styles/style.css";
-import "dizeto/styles/lightbox.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
