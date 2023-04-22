4c768f6a3cfc86847076a33e86a6618be122b4d1
= <intanmeilanienugraha@gmail.com>
2023-04-22 22:46:20 +0700

    Copy Data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,8 @@
+import React from "react";
+import "dizeto/styles/tailwind.css";
+import "dizeto/styles/style.css";
+import "dizeto/styles/lightbox.min.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
